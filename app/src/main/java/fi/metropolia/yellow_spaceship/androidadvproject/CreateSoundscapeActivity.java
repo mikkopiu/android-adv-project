06e0c2c6780ad76bbb77f1a77eb70970a5bc5ace
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-10-18 16:15:48 +0300

    Added FAB menu to create-view, stub click listeners

--- app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/CreateSoundscapeActivity.java
+++ app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/CreateSoundscapeActivity.java
@@ -1,17 +1,16 @@
 package fi.metropolia.yellow_spaceship.androidadvproject;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
-import fi.metropolia.yellow_spaceship.androidadvproject.menu.DrawerMenu;
+import com.github.clans.fab.FloatingActionMenu;
+import com.github.clans.fab.FloatingActionButton;
 
-/**
- * Created by Petri on 12.9.2015.
- */
 public class CreateSoundscapeActivity extends AppCompatActivity {
 
     @Override
@@ -22,6 +21,12 @@ public class CreateSoundscapeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.create_soundscape_title));
 
+        Toolbar bottomToolbar = (Toolbar) findViewById(R.id.bottom_toolbar);
+        ViewCompat.setElevation(bottomToolbar, 4.0f);
+
+        FloatingActionMenu menu = (FloatingActionMenu) findViewById(R.id.add_menu);
+        ViewCompat.setElevation(menu, 6.0f);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -30,6 +35,25 @@ public class CreateSoundscapeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 CreateSoundscapeActivity.this.onBackPressed();
+            }
+        });
+
+        FloatingActionButton fabRecBtn = (FloatingActionButton) findViewById(R.id.menu_item_record);
+        FloatingActionButton fabLibBtn = (FloatingActionButton) findViewById(R.id.menu_item_library);
+
+        fabRecBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: open library with Intent for Recordings
+                Toast.makeText(getApplicationContext(), "Record sound clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fabLibBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: open library with Intent for sounds
+                Toast.makeText(getApplicationContext(), "Sound library clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
