9dfd0f981eca187e7f19498b23159bf7d81303f3
petrior <petri.raut@gmail.com>
2015-10-28 13:35:29 +0200

    Saving mono instead of stereo to recorded files' header

--- app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/sounds/SoundRecorder.java
+++ app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/sounds/SoundRecorder.java
@@ -75,7 +75,7 @@ public class SoundRecorder {
                 long subChunk1Size = 16;
                 int bitsPerSample = 16;
                 int format = 1;
-                long channels = 2;
+                long channels = 1;
                 long sampleRate = (long)SAMPLE_RATE;
                 long byteRate = sampleRate * channels * bitsPerSample / 8;
                 int blockAlign = (int)(channels * bitsPerSample / 8);
