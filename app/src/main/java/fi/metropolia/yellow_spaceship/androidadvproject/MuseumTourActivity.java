6a2d9c1154233f27174f51fb317428ab4a80f37d
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2015-11-21 13:02:33 +0200

    Layout cleanup

--- app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/MuseumTourActivity.java
+++ app/src/main/java/fi/metropolia/yellow_spaceship/androidadvproject/MuseumTourActivity.java
@@ -1,30 +1,17 @@
 package fi.metropolia.yellow_spaceship.androidadvproject;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MuseumTourActivity extends AppCompatActivity {
-
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_museum_tour);
 
-        // Toolbar and menus + menu click events
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.getMenu().getItem(3).setChecked(true);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.museum_tour_title));
-
-//        DrawerMenu drawerMenu = new DrawerMenu(this, navigationView, drawerLayout, toolbar);
-//        drawerMenu.createMenu();
     }
 }
